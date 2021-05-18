ce4e25c1a63549e2fdb5fc9744b06a692d528288
Scott Hammel <scott_hammel@icloud.com>
2021-05-18 12:52:03 -0500

    added new files.

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+// Use d3.select to tell JavaScript to look for <tbody> tags in the HTML
+var tbody = d3.select("tobdy");
+
+// Simple JavaScript console.log statement
+// Remember that the JavaScript syntax uses console.log instead of print
+function printHello() {
+    console.log("Hello there!");
+}
+
